ff23bc8ee1b6ebdb6ba7fd1f1691e544fa8c9abc
SMIT-2411 <smit24@icloud.com>
2023-09-19 22:03:33 +0530

    Initial Commit

---
+++ LNMIIT HostelHub/LNMIIT_HostelHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LNMIIT_HostelHubApp.swift
+//  LNMIIT HostelHub
+//
+//  Created by Smit Patel on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LNMIIT_HostelHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
