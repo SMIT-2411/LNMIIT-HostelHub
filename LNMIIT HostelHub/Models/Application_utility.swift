a5a9602b1e7f5ce6393a807b2ce3149afcc8bb07
SMIT-2411 <smit24@icloud.com>
2023-11-01 04:27:46 +0530

    // added google login

---
+++ LNMIIT HostelHub/Models/Application_utility.swift
@@ -0,0 +1,8 @@
+//
+//  Application_utility.swift
+//  LNMIIT HostelHub
+//
+//  Created by Smit Patel on 01/11/23.
+//
+
+import Foundation
---
+++ LNMIIT HostelHub/Models/SignIn_withGoogle.swift
@@ -0,0 +1,8 @@
+//
+//  SignIn_withGoogle.swift
+//  LNMIIT HostelHub
+//
+//  Created by Smit Patel on 01/11/23.
+//
+
+import Foundation
